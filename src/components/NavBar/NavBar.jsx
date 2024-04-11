900a7b08e8e7b591d4d34fc053b125053117b221
xixismile <gaoleiccie@gmail.com>
2024-04-11 22:54:52 +1000

    update navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,11 +1,44 @@
 import Button from "../Button/Button";
+import { useState, useEffect } from "react";
+import { Link } from "react-scoll";
 import classes from "./NavBar.module.css";
 
-export default function Navbar() { 
-    return (
-        <section>
-            <p>this is Navbar section</p>
-            <Button className="btn-outline-primary">Contact Me</Button>
-        </section>
-    )
+export default function Navbar() {
+  const [navActive, setNavActive] = useEffect(false);
+
+  const toggleNav = () => {
+    setNavActive(!navActive);
+  };
+
+  const closeMenu = () => {
+    setNavActive(false);
+  };
+
+  useEffect(() => {
+    const resizeHandle = () => {
+      if (window.innerWidth <= 500) {
+        closeMenu;
+      }
+    };
+
+    window.addEventListener("resize", resizeHandle);
+
+    return () => {
+      window.removeEventListener("resize", resizeHandle);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (window.innerWidth <= 1200) {
+      closeMenu;
+    }
+  }, []);
+
+  return (
+    <nav className={`navbar ${navActive ? "active" : ""}`}>
+      <div>
+        <img src="../../assets/logo.png" alt="Logo" />
+      </div>
+    </nav>
+  );
 }
