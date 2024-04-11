34c16212fe7ba300b0fdeeb36924c078524f3a8e
xixismile <gaoleiccie@gmail.com>
2024-04-11 23:33:28 +1000

    update navbar Link lists, add routes in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import "./App.css";
 import Home from "./Pages/Home/Homescreen/HomeScreen";
+import Navbar from "./components/NavBar/NavBar";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Router>
+        <div>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="*" element={ <div>404 NOT FOUND</div>}></Route>
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,10 +1,11 @@
 import Button from "../Button/Button";
 import { useState, useEffect } from "react";
-import { Link } from "react-scoll";
+import { Link } from "react-scroll";
 import classes from "./NavBar.module.css";
+import logo from "../../assets/logo.svg";
 
 export default function Navbar() {
-  const [navActive, setNavActive] = useEffect(false);
+  const [navActive, setNavActive] = useState(false);
 
   const toggleNav = () => {
     setNavActive(!navActive);
@@ -37,8 +38,87 @@ export default function Navbar() {
   return (
     <nav className={`navbar ${navActive ? "active" : ""}`}>
       <div>
-        <img src="../../assets/logo.png" alt="Logo" />
+        <img src={logo} alt="Logo" />
       </div>
+      <a className={`nav__hamburger ${navActive ? "active" : ""} `}>
+        <span className="nav__hamburger--line"></span>
+        <span className="nav__hamburger--line"></span>
+        <span className="nav__hamburger--line"></span>
+      </a>
+      <div className={`navbar__items ${navActive ? "active" : ""}`}>
+        <ul>
+          <li>
+            <Link
+              onClick={closeMenu}
+              activeClass="navbar-active__content"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+              to="heroSection"
+              className="navbar__content"
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={closeMenu}
+              activeClass="navbar-active__content"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+              to="MyPortfolio"
+              className="navbar__content"
+            >
+              Portfolio
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={closeMenu}
+              activeClass="navbar-active__content"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+              to="AboutMe"
+              className="navbar__content"
+            >
+              About Me
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={closeMenu}
+              activeClass="navbar-active__content"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+              to="Testimonals"
+              className="navbar__content"
+            >
+              Testimonals
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <Button>
+        <Link
+          onClick={closeMenu}
+          activeClass="navbar-active__content"
+          spy={true}
+          smooth={true}
+          offset={-70}
+          duration={500}
+          to="Contact"
+          className="navbar__content"
+        >
+          Contact Me
+        </Link>
+      </Button>
     </nav>
   );
 }
